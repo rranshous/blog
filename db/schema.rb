5098fc5b3ebcd0c254fb648fb39f5c2ce47b1aab
Robby Ranshous <rranshous@gmail.com>
2014-02-19 20:39:09 -0500

    added email address to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140219233227) do
+ActiveRecord::Schema.define(version: 20140220010956) do
 
   create_table "users", force: true do |t|
     t.integer  "age"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140219233227) do
     t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email_address"
   end
 
 end
